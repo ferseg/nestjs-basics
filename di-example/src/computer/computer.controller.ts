191d3feb63b3c413a7d5d292eefbfabaa7f2d8f3
Fernando Segovia <fernandosegovia25@gmail.com>
2023-04-11 13:40:36 -0600

    Latest changes

---
+++ di-example/src/computer/computer.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+
+@Controller('computer')
+export class ComputerController {
+  constructor(
+    private readonly cpuService: CpuService,
+    private readonly diskService: DiskService,
+  ) {}
+
+  @Get()
+  run() {
+    return [this.cpuService.compute(2, 5), this.diskService.getData()];
+  }
+}
